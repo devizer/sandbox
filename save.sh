483e5cb75f0056d7b0c9799d72e215db06aea914
devizer <noreply@devizer.sb>
2016-07-17 16:21:58 +0300

    update

---
+++ save.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git config credential.helper store
+git config --global user.name "devizer"
+git config --global user.email "noreply@devizer.sb"
+git pull
+
+git commit -am "update"
+git push
+
