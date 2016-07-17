1109524b382ee74eceea73fca3fb55acbc742f18
devizer <noreply@devizer.sb>
2016-07-17 17:27:02 +0300

    update

--- nuget-for-old-mono/test.sh
+++ nuget-for-old-mono/test.sh
@@ -1,1 +1,1 @@
-./nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory testrunner
+MONO_LOG_LEVEL=debug ./nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory testrunner
--- save.sh
+++ save.sh
@@ -3,7 +3,7 @@ git config credential.helper store
 git config --global user.name "devizer"
 git config --global user.email "noreply@devizer.sb"
 git pull
-git add -all
+git add --all
 
 git commit -am "update"
 git push
